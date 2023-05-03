4cbd6d9dbacf35bf36c007af66fba6f55f4913c5
Mohsen Kashefikia <mohsen.kashefi77@gmail.com>
2023-05-03 12:24:59 +0330

    Create Our Models

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    username = models.CharField(max_length=255)
+    credit = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=255)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Invoic(models.Model):
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
