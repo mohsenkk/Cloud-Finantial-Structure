14ee51f42d45353a5ddc3755889f3f048c0ae536
Mohsen Kashefikia <mohsen.kashefi77@gmail.com>
2023-05-04 22:38:26 +0330

    Extend Customer Model with OneToOne relation

--- subscription/models.py
+++ subscription/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=255)
     credit = models.DecimalField(max_digits=10, decimal_places=2)
 
