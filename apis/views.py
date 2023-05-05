c26f17ed519352507998eca435fb985d674bb578
Mohsen Kashefikia <mohsen.kashefi77@gmail.com>
2023-05-05 08:38:05 +0330

    Create subscription API

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators  import api_view, authentication_classes, permission_classes
+from subscription.models import Customer, Subscription, Invoic
+from .serializers import SubscriptionSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def subscription_view(request, format=None):
+    if request.method == 'GET':
+        subscriptions = Customer.objects.filter(pk=request.user.id).select_related('subscriptions').all()
+        serializer = SubscriptionSerializer(
+            subscriptions, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SubscriptionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
