1d9d5de122bcb95e4ebb41bc5671d41b2668c6cb
Mohsen Kashefikia <mohsen.kashefi77@gmail.com>
2023-05-05 08:09:05 +0330

    Change related_name for Reverse Relationships of Models

--- subscription/models.py
+++ subscription/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
     username = models.CharField(max_length=255)
     credit = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -11,9 +11,9 @@ class Subscription(models.Model):
     name = models.CharField(max_length=255)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField()
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='subscriptions')
 
 class Invoic(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
-    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, related_name='invoices')
